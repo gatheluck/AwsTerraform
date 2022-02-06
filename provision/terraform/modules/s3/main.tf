5433f867847fa1345c8a1f35e98012ed9795c175
gatheluck <gatheluck+tech@gmail.com>
2022-02-06 11:50:07 +0900

    style: apply formatter.

--- provision/terraform/modules/s3/main.tf
+++ provision/terraform/modules/s3/main.tf
@@ -1,22 +1,22 @@
 resource "aws_s3_bucket" "file_storage" {
-    bucket = "${var.root_name}-${var.project_name}"
-    acl    = "private"
+  bucket = "${var.root_name}-${var.project_name}"
+  acl    = "private"
 
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
+  }
 
-    force_destroy = var.s3_force_destroy
+  force_destroy = var.s3_force_destroy
 }
 
 resource "aws_s3_bucket_public_access_block" "file_storage" {
-    bucket                  = aws_s3_bucket.file_storage.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.file_storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- provision/terraform/modules/singleton/versions.tf
+++ provision/terraform/modules/singleton/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = ">= 1.1.3"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~>3.71.0"
-        }
+  required_version = ">= 1.1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.71.0"
     }
+  }
 }
