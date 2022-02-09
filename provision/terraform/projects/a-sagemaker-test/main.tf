aa24f83cadbc20c7fcad6e5c6c6be5f8f0c981bc
gatheluck <gatheluck+tech@gmail.com>
2022-02-10 01:01:14 +0900

    feat: use iam and sagemaker modules.

--- provision/terraform/projects/a-sagemaker-test/main.tf
+++ provision/terraform/projects/a-sagemaker-test/main.tf
@@ -2,10 +2,30 @@ locals {
   project_name = "a-sagemaker-test"
 }
 
-module "a_sagemaker_test_s3" {
+module "file_storage" {
   source = "../../modules/s3"
 
-  root_name        = var.root_name
+  # vavriables
   project_name     = local.project_name
+  root_name        = var.root_name
   s3_force_destroy = true
+}
+
+module "iam" {
+  source = "../../modules/iam"
+
+  # variables
+  aws_region = var.aws_region
+  identifier = "sagemaker.amazonaws.com"
+}
+
+module "notebook" {
+  source = "../../modules/sagemaker"
+
+  # vavriables
+  notebook_instance_type = "ml.t2.medium"
+  notebook_volume_size   = 25
+  project_name           = local.project_name
+  role_arn               = module.iam.iam_role_arn
+  root_name              = var.root_name
 }
